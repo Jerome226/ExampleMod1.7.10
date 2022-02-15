223eb3e84361cbb5afd23a10b8225dbdda2aada6
Léa Gris <lea.gris@noiraude.net>
2022-02-15 21:25:53 +0100

    fix(script): shellcheck and refactor

--- .github/scripts/update-version.sh
+++ .github/scripts/update-version.sh
@@ -1,10 +1,12 @@
-if [ ! -z $(git diff --name-only HEAD HEAD~1 | grep build.gradle) ]; then
+#!/usr/bin/env bash
+
+if ! git diff --name-only HEAD HEAD~1 | grep -qF 'build.gradle'; then
   new_version="$(date +%s)"
   sed --in-place "s!^//version:.*!//version: $new_version!g" build.gradle
   git add build.gradle
   git commit -m "[ci skip] update build script version to $new_version"
   git push
-  echo "Updated buildscript version to $new_version"; 
+  printf 'Updated buildscript version to %s\n' "$new_version"
 else
-  echo "Ignored buildscript version update: no changes detected"
+  printf 'Ignored buildscript version update: no changes detected\n'
 fi
